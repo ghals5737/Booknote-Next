a16b2e4ae0f93dfaaf0d6589f16b01e0009bb250
yoooyeon <yooyeon.dev@gmail.com>
2025-12-20 21:54:08 +0900

    fix: height

--- components/library/my-library.tsx
+++ components/library/my-library.tsx
@@ -89,7 +89,7 @@ export function MyLibrary({ books }: { books: UserBookResponse[] }) {
           ))}
       </div>
 
-      <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3 items-stretch">
         {filteredBooks.map((book) => (
           <LibraryBookCard key={book.id} book={book} />
         ))}
